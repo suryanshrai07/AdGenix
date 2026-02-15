63edab48153b7b71f7b85f6966844730e7187773
suryanshrai07 <suryanshrai1187@gmail.com>
2026-02-15 23:44:46 +0530

    add routing and new pages for generator, result, my generation, community, plans, and loading

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,13 @@ import Home from './pages/Home';
 import SoftBackdrop from './components/SoftBackdrop';
 import Footer from './components/Footer';
 import LenisScroll from './components/lenis';
+import { Route, Routes } from 'react-router-dom';
+import Generator from './pages/Generator';
+import Result from './pages/Result';
+import MyGeneration from './pages/MyGeneration';
+import Community from './pages/Community';
+import Plans from './pages/Plans';
+import Loading from './pages/Loading';
 
 function App() {
 	return (
@@ -10,7 +17,17 @@ function App() {
 			<SoftBackdrop />
 			<LenisScroll />
 			<Navbar />
-			<Home />
+
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/generate" element={<Generator />} />
+				<Route path="/result/:projectId" element={<Result />} />
+				<Route path="/my-generations" element={<MyGeneration/>} />
+				<Route path="/community" element={<Community/>} />
+				<Route path="/plans" element={<Plans/>} />
+				<Route path="/loading" element={<Loading/>} />
+			</Routes>
+			
 			<Footer />
 		</>
 	);
---
+++ frontend/src/pages/MyGeneration.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MyGeneration = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default MyGeneration
