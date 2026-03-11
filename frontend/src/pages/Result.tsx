a16acc2915fd1e8068dabba15e755dd9d1892fc4
suryanshrai07 <suryanshrai1187@gmail.com>
2026-03-12 01:41:15 +0530

    update Result page with project data fetching and loading state; add baseline-browser-mapping dependency

--- frontend/src/pages/Result.tsx
+++ frontend/src/pages/Result.tsx
@@ -1,11 +1,78 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import type { Project } from "../types";
+import { dummyGenerations } from "../assets/assets";
+import { Loader2Icon, RefreshCcwIcon } from "lucide-react";
+import { Link } from "react-router-dom";
 
 const Result = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [project, setProjectData] = useState<Project>({} as Project);
+  const [loading, setLoading] = useState(true);
+  const [isGenerating, setIsGenerating] = useState(false);
 
-export default Result
+  const fetchProjectData = async () => {
+    setTimeout(() => {
+      setProjectData(dummyGenerations[0]);
+      setLoading(false);
+    }, 3000);
+  };
+
+  useEffect(() => {
+    fetchProjectData();
+  }, []);
+
+  return loading ? (
+    <div className="h-screen w-full flex items-center justify-center">
+      <Loader2Icon className="animate-spin text-indigo- size-9" />
+    </div>
+  ) : (
+    <div className="min-h-screen text-white p-6 md:p-12 mt-20">
+      <div className="max-w-6xl mx-auto">
+        <header className="flex justify-between items-center mb-8">
+          <h1 className="text-2xl md:text-3xl font-medium">
+            Generation Result
+          </h1>
+          <Link
+            to="/generate"
+            className="btn-secondary text-sm flex items-center gap-2"
+          >
+            <RefreshCcwIcon className="w-4 h-4" />
+            <p className="max-sm:hidden">New Generation</p>
+          </Link>
+        </header>
+
+        {/* grid layout */}
+        <div className="grid lg:grid-cols-3 gap-8">
+          {/* Main Result Display */}
+          <div className="lg:col-span-2 space-y-6">
+            <div className="glass-panel inline-block p-2 rounded-2xl">
+              <div
+                className={`${project?.aspectRatio === "9:16" ? "aspect-9/16" : "aspect-video"} sm:max-h-200 rounded-xl bg-gray-900 overflow-hidden relative`}
+              >
+                {project?.generatedVideo ? (
+                  <video
+                    src={project.generatedVideo}
+                    controls
+                    autoPlay
+                    loop
+                    muted
+                  />
+                ) : (
+                  <img
+                    src={project.generatedImage}
+                    alt="Generated result"
+                    className="w-full h-full object-cover"
+                  />
+                )}
+              </div>
+            </div>
+          </div>
+
+          {/* Sidebar Actions */}
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Result;
