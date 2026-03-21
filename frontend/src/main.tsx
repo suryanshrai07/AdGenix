36740871807baa0b3e8274b05fea2193a12c0736
suryanshrai07 <suryanshrai1187@gmail.com>
2026-03-21 23:59:13 +0530

    add Clerk integration with environment variable setup and update Navbar for user authentication

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,30 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App'
-import { BrowserRouter } from 'react-router-dom';
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { ClerkProvider } from "@clerk/react";
+import { dark } from "@clerk/themes";
+const publishableKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
-createRoot(document.getElementById('root')! as HTMLElement).render(
+if (!publishableKey) {
+  throw new Error(
+    "VITE_CLERK_PUBLISHABLE_KEY is not defined in the environment variables.",
+  );
+}
+
+createRoot(document.getElementById("root")! as HTMLElement).render(
+  <ClerkProvider
+    appearance={{
+      theme: dark,
+      variables: {
+        colorPrimary: "#4f39f6",
+        colorTextOnPrimaryBackground: "#ffffff",
+      },
+    }}
+    publishableKey={publishableKey}
+  >
     <BrowserRouter>
-        <App />
+      <App />
     </BrowserRouter>
-)
+  </ClerkProvider>,
+);
