6766255ebafdfb52fe19ecda101a45d3babe03b8
suryanshrai07 <suryanshrai1187@gmail.com>
2026-04-28 23:16:43 +0530

    creating endpoint for clerk webhook api

--- backend/middlewares/auth.ts
+++ backend/middlewares/auth.ts
@@ -2,7 +2,7 @@ import { Request,Response,NextFunction } from "express";
 
 export const protect = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const {userId} = req.auth();
+        const { userId } = req.auth();
 
         if(!userId){
             return res.status(401).json({message: "Unauthorized"});
--- backend/server.ts
+++ backend/server.ts
@@ -1,20 +1,24 @@
-import express , {Request,Response} from 'express';
+import express, { Request, Response } from 'express';
 import cors from 'cors';
 import 'dotenv/config';
-import { clerkMiddleware } from '@clerk/express'
-
+import { clerkMiddleware } from '@clerk/express';
+import clerkWebhooks from './controllers/clerk.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
+
+
+app.post("/api/clerk", express.raw({ type: 'application/json' }), clerkWebhooks);
+
 app.use(express.json());
-app.use(clerkMiddleware());
+app.use(clerkMiddleware()); // ✅ moved before routes
 
-app.get('/',(req: Request,res : Response) => {
+app.get('/', (req: Request, res: Response) => {
     res.send("Hello World!");
-})
+});
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
-})
+});
