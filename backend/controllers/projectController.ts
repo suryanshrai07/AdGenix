ea120e6c8715c25c77765eee469fcfe76e3d309d
suryanshrai07 <suryanshrai1187@gmail.com>
2026-05-18 18:09:17 +0530

    feat: add image upload functionality and new dependencies
    - Added @google/genai and cloudinary to package.json for enhanced image processing.
    - Integrated multer for handling multipart/form-data in projectRoutes.
    - Updated createProject route to support image uploads.

--- backend/controllers/projectController.ts
+++ backend/controllers/projectController.ts
@@ -1,36 +1,214 @@
-import { Request,Response } from "express";
+import { Request, Response } from "express";
 import * as Sentry from "@sentry/node";
+import { prisma } from "../configs/prisma.js";
+import { v2 as cloudinary } from "cloudinary";
+import {
+  GenerateContentConfig,
+  HarmCategory,
+  HarmBlockThreshold,
+  
+} from "@google/genai";
+import fs from "fs";
+import ai from "../configs/ai.js";
+
+
+const loadImage = (path: string, mimetype: string) => {
+  return {
+    inlineData: {
+      data: fs.readFileSync(path).toString("base64"),
+      mimetype,
+    },
+  };
+};
 
 export const createProject = async (req: Request, res: Response) => {
-    try {
-        
-    } catch (error: any) {
-        Sentry.captureException(error);
-        return res.status(500).json({ error: error.code || error.message });
+  let tempProjectId: string = "";
+  const { userId } = req.auth();
+  let isCreditsDeducted = false;
+
+  const {
+    name = "New Project",
+    aspectRatio,
+    userPrompt,
+    productName,
+    productDescription,
+    targetLength = 5,
+  } = req.body;
+
+  const images = req.files as Express.Multer.File[] | undefined;
+
+  if (!images || images.length < 2 || !productName) {
+    return res
+      .status(400)
+      .json({ error: "Images and product name are required" });
+  }
+
+  const user = await prisma.user.findUnique({
+    where: { id: userId },
+  });
+
+  if (!user || user.credits < 5) {
+    return res.status(401).json({ error: "Insufficient credits" });
+  } else {
+    await prisma.user
+      .update({
+        where: { id: userId },
+        data: { credits: user.credits - 5 },
+      })
+      .then(() => {
+        isCreditsDeducted = true;
+      });
+  }
+  try {
+    let uploadedImages = await Promise.all(
+      images.map(async (image) => {
+        let result = await cloudinary.uploader.upload(image.path, {
+          resource_type: "image",
+        });
+        return result.secure_url;
+      }),
+    );
+
+    const project = await prisma.project.create({
+      data: {
+        name,
+        userId,
+        productName,
+        productDescription,
+        userPrompt,
+        aspectRatio,
+        targetLength: parseInt(targetLength),
+        uploadedImages,
+        isGenerating: true,
+      },
+    });
+
+    tempProjectId = project.id;
+
+    const model = "gemini-3-pro-image-preview";
+
+    const generationConfig: GenerateContentConfig = {
+      maxOutputTokens: 32768,
+      temperature: 1,
+      topP: 0.95,
+      responseModalities: ["IMAGE"],
+      imageConfig: {
+        aspectRatio: aspectRatio || "9:16",
+        imageSize: "1K",
+      },
+      safetySettings: [
+        {
+          category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+          threshold: HarmBlockThreshold.OFF,
+        },
+        {
+          category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+          threshold: HarmBlockThreshold.OFF,
+        },
+        {
+          category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+          threshold: HarmBlockThreshold.OFF,
+        },
+        {
+          category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+          threshold: HarmBlockThreshold.OFF,
+        },
+      ],
+    };
+
+    const img1base64 = loadImage(images[0].path, images[0].mimetype);
+    const img2base64 = loadImage(images[1].path, images[1].mimetype);
+
+    const prompt = {
+      text: `Combine the person and product into a realistic photo.
+      Make the person naturally hold or use the product.
+      Match lighting, shadows, scale and perspective.
+      Make the person stand in professional studio lighting.
+      Output ecommerce-quality photo realistic imagery.
+      ${userPrompt}`,
+    };
+
+    // Generate the image using AI model
+    const response: any = await ai.models.generateContent({
+      model,
+      contents: [img1base64, img2base64, prompt],
+      config: generationConfig,
+    });
+
+    if (!response?.candidates?.[0]?.content?.parts) {
+      throw new Error("AI generation failed");
     }
-}
+
+    const parts = response.candidates[0].content.parts;
+
+    let finalBuffer: Buffer | null = null;
+
+    for (const part of parts) {
+      if (part.inlineData) {
+        finalBuffer = Buffer.from(part.inlineData.data, "base64");
+      }
+    }
+
+    if (!finalBuffer) {
+      throw new Error("Failed to generate image");
+    }
+
+    const base64Image = `data:image/png;base64,${finalBuffer.toString("base64")}`;
+
+    const uploadResult = await cloudinary.uploader.upload(base64Image, {
+      resource_type: "image",
+    });
+
+    await prisma.project.update({
+      where: { id: project.id },
+      data: {
+        generatedImage: uploadResult.secure_url,
+        isGenerating: false,
+      },
+    });
+
+    return res.json({ projectId: project.id });
+  } catch (error: any) {
+    if (tempProjectId) {
+      await prisma.project.update({
+        where: { id: tempProjectId },
+        data: { isGenerating: false, error: error.message },
+      });
+    }
+
+    if(isCreditsDeducted) {
+      await prisma.user.update({
+        where : { id: userId },
+        data : {
+          credits : {increment : 5
+          }
+        }
+      })
+    }
+
+    Sentry.captureException(error);
+    return res.status(500).json({ error: error.code || error.message });
+  }
+};
 
 export const createVideo = async (req: Request, res: Response) => {
-    try {
-        
-    } catch (error: any) {
-        Sentry.captureException(error);
-        return res.status(500).json({ error: error.code || error.message });
-    }
-}
+  try {
+  } catch (error: any) {
+    Sentry.captureException(error);
+    return res.status(500).json({ error: error.code || error.message });
+  }
+};
 export const getAllPublishedProjects = async (req: Request, res: Response) => {
-    try {
-        
-    } catch (error: any) {
-        Sentry.captureException(error);
-        return res.status(500).json({ error: error.code || error.message });
-    }
-}
+  try {
+  } catch (error: any) {
+    Sentry.captureException(error);
+    return res.status(500).json({ error: error.code || error.message });
+  }
+};
 export const deleteProject = async (req: Request, res: Response) => {
-    try {
-        
-    } catch (error: any) {
-        Sentry.captureException(error);
-        return res.status(500).json({ error: error.code || error.message });
-    }
-}
+  try {
+  } catch (error: any) {
+    Sentry.captureException(error);
+    return res.status(500).json({ error: error.code || error.message });
+  }
+};
--- backend/routes/projectRoutes.ts
+++ backend/routes/projectRoutes.ts
@@ -1,10 +1,21 @@
 import express from "express";
-import { createProject, createVideo, deleteProject, getAllPublishedProjects } from "../controllers/projectController.js";
+import {
+  createProject,
+  createVideo,
+  deleteProject,
+  getAllPublishedProjects,
+} from "../controllers/projectController.js";
 import { protect } from "../middlewares/auth.js";
+import upload from "../configs/multer.js";
 
 const projectRouter = express.Router();
 
-projectRouter.post("/create", protect, createProject);
+projectRouter.post(
+  "/create",
+  upload.array("images", 2),
+  protect,
+  createProject,
+);
 projectRouter.post("/video", protect, createVideo);
 projectRouter.get("/published", protect, getAllPublishedProjects);
 projectRouter.delete("/:projectId", protect, deleteProject);
